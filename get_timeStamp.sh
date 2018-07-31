7d4e57310110c42b8c6c3821aaa45e36dee4c3f5
vimi <vimiming@gmail.com>
2018-07-31 11:13:46 +0800

    [TimeStamp] get current timeStamp

---
+++ get_timeStamp.sh
@@ -0,0 +1,8 @@
+
+
+current=`date "+%Y-%m-%d %H:%M:%S"`  
+timeStamp=`date -d "$current" +%s`   
+#将current转换为时间戳，精确到毫秒  
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
+currentTimeStamp=$currentTimeStamp".vimi.bck"
+echo $currentTimeStamp
