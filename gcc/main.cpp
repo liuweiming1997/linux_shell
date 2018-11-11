14a0d9c88a4976f11ae1cfbdb9475f7831aa3243
vimi <vimiming@gmail.com>
2018-11-11 12:18:15 +0800

    可以构建cpp

--- gcc/main.cpp
+++ gcc/main.cpp
@@ -1,9 +1,11 @@
 #include "function/mytool1.h"
 #include "function/mytool2.h"
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
 	mytool1_print("hello mytool1!");
 	mytool2_print("hello mytool2!");
+  cout << "fuck" << endl;
 	return 0;
 }
 
